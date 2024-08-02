e4e2b35cfb3f40a4857c77b767a1ca882f563c1a
Leewonno <dldnjssh123@naver.com>
2024-08-02 11:07:28 +0900

    temp

--- src/main/components/widget/TranslateWidget.tsx
+++ src/main/components/widget/TranslateWidget.tsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react"
+
 export function TranslateWidget(){
+
+    const [lyrics, setLyrics] = useState<string>("")
+
+    useEffect(()=>{
+        console.log(lyrics)
+    }, [lyrics])
+
     return (
         <>
             <form>
-                <textarea placeholder="가사를 입력하세요"/>
+                <textarea placeholder="가사를 입력하세요" value={lyrics} onChange={(e)=>setLyrics(e.target.value)}/>
                 ▶
                 <textarea readOnly />
             </form>
