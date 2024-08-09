608777b1f80696cdda00a5c1b334e61b7dcafe55
Leewonno <dldnjssh123@naver.com>
2024-08-09 11:36:16 +0900

    [Feat] 변환된 데이터 초 -> 시간:분:초

--- src/main/components/widget/TranslateWidget.tsx
+++ src/main/components/widget/TranslateWidget.tsx
@@ -10,14 +10,14 @@ export function TranslateWidget() {
 
   const [message, setMessage] = useState<string>("")
 
-  const toast = useSelector((state) => state.visible.value)
-  const dispatch = useDispatch();
+  // const toast = useSelector((state) => state.visible.value)
+  // const dispatch = useDispatch();
   
 
   const translateCopy = () => {
     navigator.clipboard.writeText(translation);
     setTimeout(()=>{
-      dispatch()
+      // dispatch()
     }, 5000)
     setMessage("복사되었습니다!")
   }
@@ -52,7 +52,18 @@ export function TranslateWidget() {
     content.map((value, index) => {
       const idx: number = index + 1;
       const first: string = idx.toString().concat("\n");
-      const second: string = `00:${index * viewTime}:00 --> 00:${index * viewTime + viewTime}:00`.concat("\n");
+      const view_time_origin = index * viewTime;
+      const view_time_next = index * viewTime + viewTime;
+
+      const origin_hour = Math.floor(view_time_origin/3600)
+      const origin_mins = Math.floor((view_time_origin - origin_hour*3600)/60)
+      const origin_secs = view_time_origin - origin_hour*3600 - origin_mins*60
+
+      const next_hour = Math.floor(view_time_next/3600)
+      const next_mins = Math.floor((view_time_next - next_hour*3600)/60)
+      const next_secs = view_time_next - next_hour*3600 - next_mins*60
+
+      const second: string = `${origin_hour}:${origin_mins}:${origin_secs}:00 --> ${next_hour}:${next_mins}:${next_secs}:00`.concat("\n");
       const third: string = value.concat("\n");
       const sum: string = first + second + third;
       const temp: string = translation + sum;
