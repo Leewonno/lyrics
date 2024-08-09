48584a809259f84de8f3806909b9a3c1435a243f
Leewonno <dldnjssh123@naver.com>
2024-08-09 13:30:32 +0900

    [Feat] css 설정 및 파이어베이스 패키지 추가

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,4 +1,7 @@
 import { Translate } from "./components/Translate";
+import style from './css/main.module.css'
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
 
 export function Main(){
     return (
