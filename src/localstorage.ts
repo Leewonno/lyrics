11135f81a6e5d6ee5b56681e3eea80d52dbfbcf5
Leewonno <dldnjssh123@naver.com>
2024-08-10 22:29:56 +0900

    [Feat] 방문자수 체크

---
+++ src/localstorage.ts
@@ -0,0 +1,15 @@
+const setLocalStorageTimer = (key: string, value: string) => {
+    let item = value;
+
+    localStorage.setItem(key, JSON.stringify(item));
+};
+
+const getLocalStorage = (key: string) => {
+    let item = localStorage.getItem(key);
+    if (item === null) return null;
+
+    item = JSON.parse(item);
+    return item;
+};
+
+export {setLocalStorageTimer, getLocalStorage}
