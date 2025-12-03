ac89ad030a4fbd0145c382c69e54764aa888fbaf
Leewonno <dldnjssh123@naver.com>
2025-12-03 20:01:39 +0900

    feat : 앨범 페이지

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import { Main } from './main/Main';
 import { Translate } from './translate/Translate';
 import { Member } from './member/Member';
+import Album from './album/Album';
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
       {
         path: '/member',
         element: <Member />
+      },
+      {
+        path: '/album',
+        element: <Album />
       }
     ],
   },
