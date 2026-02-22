4eb26e503d147d20b7494580d0d15ce0420a2bc5
Leewonno <jsca5253@gmail.com>
2026-02-22 15:00:37 +0900

    [UPDATE] firestore 제거

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,38 +1,9 @@
-import { useEffect } from "react";
-import { doc, increment, setDoc } from "firebase/firestore";
-import { db } from "../firebase";
-import { getLocalStorage, setLocalStorageTimer } from "../localstorage";
 import { MainSection } from "./components/MainSection";
 
 export function Main() {
-
-  useEffect(() => {
-
-    const updateView = async () => {
-      const today = new Date();
-      const year = today.getFullYear();
-      const month = ('0' + (today.getMonth() + 1)).slice(-2);
-      const day = ('0' + today.getDate()).slice(-2);
-      const page = 'main';
-      const time = year + month + day;
-      const docName = + time + "_" + page;
-
-      if (getLocalStorage(page) === time) {
-        return;
-      }
-
-      setLocalStorageTimer(page, time);
-
-      const docRef = doc(db, "data", docName);
-      await setDoc(docRef, { view: increment(1) }, { merge: true });
-    }
-
-    updateView()
-  }, [])
-
   return (
     <>
       <MainSection />
     </>
-  )
+  );
 }
