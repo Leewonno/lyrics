705f64a32ead3f2d907c142f35488b1da9055bda
Leewonno <jsca5253@gmail.com>
2026-02-22 14:54:45 +0900

    [FEAT] 구글 분석 페이지뷰 기록

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom';
-import router from './Router';
-import StoreProvider from './StoreProvider';
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router";
+import StoreProvider from "./StoreProvider";
+import { initGA } from "./lib/analytics";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+if (process.env.NODE_ENV === "production") {
+  initGA();
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StoreProvider>
     <RouterProvider router={router} />
   </StoreProvider>
-)
+);
